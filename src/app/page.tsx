985e01a09732274fb96f23b7c199df0a86097bde
Daniele Rapisarda <info@danielerapisarda.com>
2024-06-15 15:05:30 +0200

    ♻️ remove switch button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,12 +31,6 @@ export default function Home() {
     });
   }
 
-  const switchTexture = () => {
-    setTextureFrontUrl(varC)
-    setTextureBackUrl(varA)
-    setTextureSideUrl(varB)
-  }
-
   const [textureFrontUrl, setTextureFrontUrl] = useState(varA);
   const [textureBackUrl, setTextureBackUrl] = useState(varB);
   const [textureSideUrl, setTextureSideUrl] = useState(varC);
@@ -65,7 +59,6 @@ export default function Home() {
             <label htmlFor="fileSide"> Side file</label>
             <input id="fileSide" type="file" onChange={handleSideFileChange} />
           </p>
-          <button onClick={switchTexture}> Switch </button>
         </div>
         {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
