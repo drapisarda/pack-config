a3441209c084cbc7e880e8bf12db1e5e52552c2b
Daniele Rapisarda <info@danielerapisarda.com>
2024-06-19 11:27:54 +0200

    🐛 ui margin fix

--- src/app/ImageSelector.tsx
+++ src/app/ImageSelector.tsx
@@ -25,7 +25,7 @@ export default function ImageSelector(props: Props) {
   return (
     <div className="image-selector flex flex-wrap md:block h-full border rounded-xl border-slate-200 p-3 md:p-5 text-black bg-white md:mb-4 md:h-auto">
       <h3 className="text-lg basis-full border-b pb-1 mx-1 md:mx-4 mb-1 md:mb-3 font-bold">{label}</h3>
-      <div className="my-2 md:my-4 basis-1/2">
+      <div className="image-selector__image my-2 md:my-4 basis-1/2">
         <Image 
           priority={true}
           className="rounded-xl max-h-32 md:max-h-64 max-w-64 m-auto h-full w-auto" 
@@ -34,11 +34,11 @@ export default function ImageSelector(props: Props) {
           src={imageSrc}
           alt="Side image" />
       </div>
-      <div className="order-4 w-full md:w-auto text-center mb:py-2 md:mb-5">
-        <label className="rounded-xl py-2 px-4 border border-slate-200 shadow-xl cursor-pointer" htmlFor={inputId}> Chose a new image </label>
+      <div className="image-selector__input order-4 w-full mt-1 md:w-auto text-center mb:py-2 md:mb-5">
+        <label className="rounded-xl inline-block py-1 px-4 border border-slate-200 shadow-xl cursor-pointer" htmlFor={inputId}> Chose a new image </label>
         <input id={inputId} type="file" onChange={(e) => onChangeHandle(e)} className="hidden"/>
       </div>
-      <p className="mt-2 md:mt-5 basis-1/2 order-3 px-2 md:px-0">
+      <p className="image-selector__description mt-2 md:mt-5 basis-1/2 order-3 px-2 md:px-0">
         {description}
       </p>
     </div>
